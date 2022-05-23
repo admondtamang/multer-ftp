a712e4f3faad002c233fab3ff66a9454995e439b
admond <admondtamang@gmail.com>
2022-05-23 23:16:06 +0545

    [add] size and fileName while returning files

--- index.js
+++ index.js
@@ -1,70 +1,102 @@
-var Promise = require('bluebird')
-var FTP = require('ftp')
-var path = require('path')
-var defaults = require('lodash.defaults')
-var random = Promise.promisify(require('crypto').randomBytes)
+var Promise = require("bluebird");
+var os = require("os");
+var FTP = require("ftp");
+var fs = require("fs");
+var path = require("path");
+var defaults = require("lodash.defaults");
+var random = Promise.promisify(require("crypto").randomBytes);
+var crypto = require("crypto");
 
-function getDestination (req, file, opts, cb) {
-  random(16).then(function(raw) {
-    cb(null, path.join(opts.basepath, raw.toString('hex') + path.extname(file.originalname)))
-  }, cb)
+function getDestination(req, file, opts, cb) {
+  random(16).then(function (raw) {
+    cb(null, path.join(opts.basepath, raw.toString("hex") + path.extname(file.originalname)));
+  }, cb);
 }
 
-function FTPStorage (opts) {
+function FTPStorage(opts) {
   this.opts = defaults(opts, {
-    basepath: '',
-    destination: getDestination
-  })
+    basepath: "",
+    destination: getDestination,
+  });
 
   if (this.opts.connection) {
-    this.ftp = this.opts.connection
-    this.ready = Promise.resolve()
+    this.ftp = this.opts.connection;
+    this.ready = Promise.resolve();
   } else {
-    this.ftp = new FTP()
-    this.ready = new Promise(function (resolve, reject) {
-      this.ftp.on('ready', resolve)
-    }.bind(this))
+    this.ftp = new FTP();
+    this.ready = new Promise(
+      function (resolve, reject) {
+        this.ftp.on("ready", resolve);
+      }.bind(this)
+    );
 
-    this.ftp.connect(this.opts.ftp)
+    this.ftp.connect(this.opts.ftp);
   }
 }
 
-FTPStorage.prototype._handleFile = function _handleFile (req, file, cb) {
-  var instance = this
+FTPStorage.prototype._handleFile = function _handleFile(req, file, cb) {
+  var instance = this;
+  var obj = {};
 
-  function handleFile (stream) {
+  function handleFile(stream) {
+    // ftp
     instance.opts.destination(req, file, instance.opts, function (err, destination) {
-      if (err) return cb(err)
+      if (err) return cb(err);
+
+      function getFilename(req, file, cb) {
+        crypto.pseudoRandomBytes(16, function (err, raw) {
+          cb(err, err ? undefined : raw.toString("hex"));
+        });
+      }
+
+      getFilename(req, file, (err, filename) => {
+        const destination = os.tmpdir();
+        if (err) return cb(err);
+
+        var finalPath = path.join(destination, filename);
+        var outStream = fs.createWriteStream(finalPath);
+
+        file.stream.pipe(outStream);
+        outStream.on("error", cb);
+        outStream.on("finish", function () {
+          obj = {
+            filename: filename,
+            size: outStream.bytesWritten,
+          };
+        });
+      }); // end of fileName
 
       instance.ftp.put(stream, destination, function (err) {
-        if (err) return cb(err)
-
+        if (err) return cb(err);
         cb(null, {
-          path: destination
-        })
-      })
-    })
+          path: destination,
+          ...obj,
+        });
+      }); //end of ftp
+    });
   }
 
   instance.ready.then(function () {
     if (instance.opts.transformFile) {
       instance.opts.transformFile(req, file, function (err, stream) {
-        if (err) return cb(err)
+        if (err) return cb(err);
 
-        file.transformedStream = stream
+        file.transformedStream = stream;
 
-        handleFile(file.transformedStream)
-      })
+        handleFile(file.transformedStream);
+      });
     } else {
-      handleFile(file.stream)
+      handleFile(file.stream);
     }
-  })
-}
+  });
+};
 
 FTPStorage.prototype._removeFile = function _removeFile(req, file, cb) {
-  this.ready.then(function () {
-    this.ftp.delete(file.path, cb)
-  }.bind(this))
-}
+  this.ready.then(
+    function () {
+      this.ftp.delete(file.path, cb);
+    }.bind(this)
+  );
+};
 
-module.exports = FTPStorage
+module.exports = FTPStorage;
